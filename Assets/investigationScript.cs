57ec657c3a96e23c286a5c2981d00701aec024d8
Blananas2 <45607696+Blananas2@users.noreply.github.com>
2026-02-07 20:34:30 -0500

    minor stuffs

--- Assets/investigationScript.cs
+++ Assets/investigationScript.cs
@@ -4,12 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UI;
 using KModkit;
+using Rnd = UnityEngine.Random;
 
 public class investigationScript : MonoBehaviour {
 
     public KMBombInfo Bomb;
     public KMAudio Audio;
+
+    public Text SearchQuery;
+    public KMSelectable SearchButton;
+    public TextMesh ResultSummary;
+    public Text[] EntryTexts;
+    public KMSelectable ScrollBar;
+    public TextMesh BottomText;
+
+    public TextAsset[] Transcripts;
+
+    int transcriptIx;
+    string chosenTranscript;
 
     //Logging
     static int moduleIdCounter = 1;
@@ -30,12 +44,10 @@ public class investigationScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-
-    }
-
-    // Update is called once per frame
-    void Update () {
-
+        transcriptIx = Rnd.Range(0, Transcripts.Length);
+        chosenTranscript = Transcripts[transcriptIx].ToString();
+        
+        //Debug.Log(Solves[transcriptIx].MissionName);
     }
 
     /*
