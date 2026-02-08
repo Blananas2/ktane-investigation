90ee812d2c79907ad2584facc8f63970a052f44c
QuinnWuest <qwuest123@gmail.com>
2026-02-07 21:01:33 -0500

    cha cha now yall

--- Assets/investigationScript.cs
+++ Assets/investigationScript.cs
@@ -8,7 +8,8 @@ using UnityEngine.UI;
 using KModkit;
 using Rnd = UnityEngine.Random;
 
-public class investigationScript : MonoBehaviour {
+public class investigationScript : MonoBehaviour
+{
 
     public KMBombInfo Bomb;
     public KMAudio Audio;
@@ -17,7 +18,9 @@ public class investigationScript : MonoBehaviour {
     public KMSelectable SearchButton;
     public TextMesh ResultSummary;
     public Text[] EntryTexts;
-    public KMSelectable ScrollBar;
+    public GameObject ScrollBarObj;
+    public KMSelectable ScrollBarSel;
+    public KMSelectable[] ScrollBarAreaSels;
     public TextMesh BottomText;
 
     public TextAsset[] Transcripts;
@@ -26,11 +29,20 @@ public class investigationScript : MonoBehaviour {
     string chosenTranscript;
 
     //Logging
-    static int moduleIdCounter = 1;
-    int moduleId;
+    private int moduleId;
+    private static int moduleIdCounter = 1;
     private bool moduleSolved;
 
-    void Awake () {
+    private bool _scrollBarHeld = false;
+    private int _currentScrollBarPos = 0;
+    private Coroutine _scrollBarAnimation;
+    private static readonly float[] _scrollBarPositions = new float[]
+    {
+        0.0375f, 0.0225f, 0.0075f, -0.0075f, -0.0225f, -0.0375f
+    };
+
+    void Awake()
+    {
         moduleId = moduleIdCounter++;
         /*
         foreach (KMSelectable object in keypad) {
@@ -39,15 +51,79 @@ public class investigationScript : MonoBehaviour {
         */
 
         //button.OnInteract += delegate () { buttonPress(); return false; };
+        ScrollBarSel.OnInteract += ScrollBarPress;
+        ScrollBarSel.OnInteractEnded += ScrollBarRelease;
+        for (int i = 0; i < ScrollBarAreaSels.Length; i++)
+        {
+            ScrollBarAreaSels[i].OnHighlight += ScrollAreaHighlight(i);
+            ScrollBarAreaSels[i].OnInteract += UnityEditorOnlyScrollAreaPress(i);
+        }
+    }
 
+    private KMSelectable.OnInteractHandler UnityEditorOnlyScrollAreaPress(int i)
+    {
+        return delegate ()
+        {
+            if (!Application.isEditor)
+                return false;
+            _currentScrollBarPos = i;
+            if (_scrollBarAnimation != null)
+                StopCoroutine(_scrollBarAnimation);
+            _scrollBarAnimation = StartCoroutine(AnimateScrollBarMovement(_currentScrollBarPos));
+            return false;
+        };
     }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        return;
         transcriptIx = Rnd.Range(0, Transcripts.Length);
         chosenTranscript = Transcripts[transcriptIx].ToString();
-        
-        //Debug.Log(Solves[transcriptIx].MissionName);
+
+
+
+    }
+
+    private bool ScrollBarPress()
+    {
+        _scrollBarHeld = true;
+        return false;
+    }
+
+    private void ScrollBarRelease()
+    {
+
+        _scrollBarHeld = false;
+
+    }
+
+    private Action ScrollAreaHighlight(int i)
+    {
+        return delegate ()
+        {
+            if (!_scrollBarHeld)
+                return;
+            _currentScrollBarPos = i;
+            if (_scrollBarAnimation != null)
+                StopCoroutine(_scrollBarAnimation);
+            _scrollBarAnimation = StartCoroutine(AnimateScrollBarMovement(_currentScrollBarPos));
+        };
+    }
+
+    private IEnumerator AnimateScrollBarMovement(int goalPos)
+    {
+        var startScrollBarPos = ScrollBarObj.transform.localPosition;
+        var goalScrollBarPosZ = _scrollBarPositions[goalPos];
+        var duration = 0.1f;
+        var elapsed = 0f;
+        while (elapsed < duration)
+        {
+            ScrollBarObj.transform.localPosition = new Vector3(startScrollBarPos.x, startScrollBarPos.y, Mathf.Lerp(startScrollBarPos.z, goalScrollBarPosZ, elapsed / duration));
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        ScrollBarObj.transform.localPosition = new Vector3(startScrollBarPos.x, startScrollBarPos.y, goalScrollBarPosZ);
     }
 
     /*
